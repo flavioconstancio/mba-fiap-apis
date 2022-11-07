b07d9cf027fbae4fee06bf4256504d48982359b5
Flávio Constâncio <flaviolc@gmail.com>
2022-11-07 19:20:48 -0300

    Delete src directory

--- src/config/cfg.js
+++
@@ -1,7 +0,0 @@
-const cfg = ()=>{
-    return {
-        jwt_secret:"flavio",
-        db:"mongodb+srv://mbafiap:qbQsExKx4hwGFtLl@clustercliente.trczskv.mongodb.net/?retryWrites=true&w=majority"
-    }
-}
-module.exports = cfg();
--- src/middleware/notfound.js
+++
@@ -1,7 +0,0 @@
-
-const notfound = (req, res, next) => {
-    res.type("application/json");
-    res.status(404).send("404 - Not Found");
-    next();
-}
-module.exports = notfound;
